c39611b2e6342da35118abe4c641812aa860c8fa
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-09-19 12:09:48 +0300

    delete unused code

--- clj/src/clj/core.clj
+++
@@ -1,7 +0,0 @@
-(ns clj.core
-  (:gen-class
-   :prefix "method-"
-   :methods [[getStringWithPrefix [String] String]]))
-
-(defn method-getStringWithPrefix [this asdf]
-      (str "prefix " asdf))
--- clj/test/clj_render/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-render.core-test
-  (:require [clojure.test :refer :all]
-            [clj-render.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
