eb5d05a13f36e10c76a9ce33be40c31208fe1121
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-09-19 12:11:41 +0300

    fix deps

--- clj/project.clj
+++ clj/project.clj
@@ -1,10 +1,10 @@
-(defproject clj "0.1.0-SNAPSHOT"
+(defproject h3m-lwp-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [h3m-parser "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [h3m-parser "1.0.1"]]
   :main h3m-lwp-clj.core
   :target-path "target/%s"
   :source-paths ["src"]
