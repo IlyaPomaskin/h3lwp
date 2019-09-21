021b9f06737c1304238bf8a19b7dfbc6db831246
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-09-21 18:09:54 +0300

    lower clojure version to 1.8.0

--- clj/project.clj
+++ clj/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [h3m-parser "1.0.1"]]
   :main h3m-lwp-clj.core
   :target-path "target/%s"
