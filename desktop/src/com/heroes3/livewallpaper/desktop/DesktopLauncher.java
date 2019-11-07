c5935d3d700f68f4f5c61fa6409ec449795a381d
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-11-07 18:51:40 +0300

    remove unused import

--- desktop/src/com/heroes3/livewallpaper/desktop/DesktopLauncher.java
+++ desktop/src/com/heroes3/livewallpaper/desktop/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.heroes3.livewallpaper.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.heroes3.livewallpaper.Heroes3LWP;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
