ef1055737513518db9abde8a979d411b374a1a93
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-11-20 17:34:27 +0300

    add build optimizations

--- clj/project.clj
+++ clj/project.clj
@@ -5,6 +5,10 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [h3m-parser "1.0.1"]]
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
+  :global-vars {clojure.core/*warn-on-reflection* true}
+  :aot :all
   :main h3m-lwp-clj.core
   :target-path "target/%s"
   :source-paths ["src"]
@@ -13,5 +17,4 @@
               [[com.badlogicgames.gdx/gdx "1.9.6"]
                [com.badlogicgames.gdx/gdx-backend-lwjgl "1.9.6"]
                [com.badlogicgames.gdx/gdx-platform "1.9.6" :classifier "natives-desktop"]]}
-   :uberjar {:aot :all
-             :uberjar-name "clj-render-standalone.jar"}})
+   :uberjar {:uberjar-name "clj-render-standalone.jar"}})
