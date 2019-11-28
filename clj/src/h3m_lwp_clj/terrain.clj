d3880f1be802d0d02d97af93b2f465d29feb42e6
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-11-29 00:56:27 +0300

    animate terrain tiles

--- clj/src/h3m_lwp_clj/terrain.clj
+++ clj/src/h3m_lwp_clj/terrain.clj
@@ -1,5 +1,5 @@
 (ns h3m-lwp-clj.terrain
-  (:import [com.badlogic.gdx.graphics.g2d SpriteCache TextureRegion])
+  (:import [com.badlogic.gdx.graphics.g2d SpriteBatch])
   (:require [h3m-lwp-clj.assets :as assets]
             [h3m-lwp-clj.rect :as rect]
             [h3m-lwp-clj.consts :as consts]))
@@ -10,49 +10,58 @@
   (get-in consts/terrain [part (get tile part)]))
 
 
-(defn create-terrain-texture-region
-  ^TextureRegion
-  [^TextureRegion images-list flip-x flip-y]
-  (doto (new TextureRegion images-list)
-    (.flip flip-x flip-y)))
-
-
 (defn render-tile
-  [^SpriteCache batch tile]
+  [^SpriteBatch batch tile]
   (let [{mirror-config :mirror-config
          x-position :x-position
          y-position :y-position
-         river :river
-         road :road
-         terrain-image-index :terrain-image-index
-         river-image-index :river-image-index
-         road-image-index :road-image-index} tile]
-    (.add
+         render-terrain :render-terrain
+         render-river :render-river
+         render-road :render-road} tile
+        flip-terrain-x (bit-test mirror-config 0)
+        flip-terrain-y (bit-test mirror-config 1)
+        flip-river-x (bit-test mirror-config 2)
+        flip-river-y (bit-test mirror-config 3)
+        flip-road-x (bit-test mirror-config 4)
+        flip-road-y (bit-test mirror-config 5)]
+    (.draw
      batch
-     (create-terrain-texture-region
-      (assets/get-terrain (tile->filename tile :terrain) terrain-image-index)
-      (bit-test mirror-config 0)
-      (bit-test mirror-config 1))
-     (float x-position)
-     (float y-position))
-    (when (pos? river)
-      (.add
+     (render-terrain)
+     (+ (float x-position) (if flip-terrain-x (- consts/tile-size) 0))
+     (+ (float y-position) (if flip-terrain-y (- consts/tile-size) 0))
+     (float consts/tile-size)
+     (float consts/tile-size)
+     (float consts/tile-size)
+     (float consts/tile-size)
+     (float (if flip-terrain-x -1 1))
+     (float (if flip-terrain-y -1 1))
+     (float 0))
+    (when (fn? render-river)
+      (.draw
        batch
-       (create-terrain-texture-region
-        (assets/get-terrain (tile->filename tile :river) river-image-index)
-        (bit-test mirror-config 2)
-        (bit-test mirror-config 3))
-       (float x-position)
-       (float y-position)))
-    (when (pos? road)
-      (.add
+       (render-river)
+       (+ (float x-position) (if flip-river-x (- consts/tile-size) 0))
+       (+ (float y-position) (if flip-river-y (- consts/tile-size) 0))
+       (float consts/tile-size)
+       (float consts/tile-size)
+       (float consts/tile-size)
+       (float consts/tile-size)
+       (float (if flip-river-x -1 1))
+       (float (if flip-river-y -1 1))
+       (float 0)))
+    (when (fn? render-road)
+      (.draw
        batch
-       (create-terrain-texture-region
-        (assets/get-terrain (tile->filename tile :road) road-image-index)
-        (bit-test mirror-config 4)
-        (bit-test mirror-config 5))
-       (float x-position)
-       (float y-position)))))
+       (render-road)
+       (+ (float x-position) (if flip-road-x (- consts/tile-size) 0))
+       (+ (float y-position) (if flip-road-y (- consts/tile-size) 0))
+       (float consts/tile-size)
+       (float consts/tile-size)
+       (float consts/tile-size)
+       (float consts/tile-size)
+       (float (if flip-road-x -1 1))
+       (float (if flip-road-y -1 1))
+       (float 0)))))
 
 
 (defn get-visible-tiles
@@ -67,9 +76,28 @@
                y (int (Math/ceil (quot i size)))
                tile (nth terrain i)]
          :when (rect/contain? x y rect)]
-     (assoc tile
-            :x-position (* (dec x) consts/tile-size)
-            :y-position (* (dec y) consts/tile-size)))))
+     (let [{river :river
+            road :road
+            terrain-image-index :terrain-image-index
+            river-image-index :river-image-index
+            road-image-index :road-image-index} tile]
+       (assoc tile
+              :x-position (* (dec x) consts/tile-size)
+              :y-position (* (dec y) consts/tile-size)
+              :render-terrain (assets/create-sprite
+                               (assets/get-terrain
+                                (tile->filename tile :terrain)
+                                terrain-image-index))
+              :render-river (when (pos? river)
+                              (assets/create-sprite
+                               (assets/get-terrain
+                                (tile->filename tile :river)
+                                river-image-index)))
+              :render-road (when (pos? road)
+                             (assets/create-sprite
+                              (assets/get-terrain
+                               (tile->filename tile :road)
+                               road-image-index))))))))
 
 
 (defn render-terrain-tiles
