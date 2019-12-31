977d99f996fa22465c3fd5307ded773c5335bcc8
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-12-31 16:09:41 +0300

    remove def parsing

--- clj/project.clj
+++ clj/project.clj
@@ -4,7 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [smee/binary "0.5.4"]
                  [h3m-parser "1.2.1"]]
   :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
