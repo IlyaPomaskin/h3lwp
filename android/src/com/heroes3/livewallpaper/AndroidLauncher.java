6a153e737e3cd4ffa2fb2fdfd286b4f58c75655f
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-01-20 20:42:50 +0300

    disable unused android abilities

--- android/src/com/heroes3/livewallpaper/AndroidLauncher.java
+++ android/src/com/heroes3/livewallpaper/AndroidLauncher.java
@@ -10,6 +10,8 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
 		initialize(new h3m.LwpCore(), config);
 	}
 }
