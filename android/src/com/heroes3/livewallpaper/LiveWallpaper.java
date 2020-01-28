b3a81eff4ae102a8e8666dbb540523a9da3fd977
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-01-28 22:30:11 +0300

    use methods for selecting files

--- android/src/com/heroes3/livewallpaper/LiveWallpaper.java
+++ android/src/com/heroes3/livewallpaper/LiveWallpaper.java
@@ -1,21 +1,52 @@
 package com.heroes3.livewallpaper;
 
-import com.badlogic.gdx.ApplicationListener;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.badlogic.gdx.backends.android.AndroidLiveWallpaperService;
 
+import h3m.LwpCore;
+
 public class LiveWallpaper extends AndroidLiveWallpaperService {
+    static String INTENT_NAME = "selectedFile";
+    static String INTENT_EXTRA_NAME = "message";
+    private LwpCore app;
+    private LiveWallpaper instance = this;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String filePath = intent.getStringExtra(INTENT_EXTRA_NAME);
+            app.setFilePath(filePath);
+        }
+    };
+
     @Override
     public void onCreateApplication() {
         super.onCreateApplication();
-        initialize(createListener());
-    }
 
-    public ApplicationListener createListener () {
-        return new h3m.LwpCore();
-    }
+        LocalBroadcastManager
+            .getInstance(this)
+            .registerReceiver(receiver, new IntentFilter(INTENT_NAME));
 
-    public AndroidApplicationConfiguration createConfig () {
-        return new AndroidApplicationConfiguration();
+        app = new h3m.LwpCore();
+        app.onFileSelectClick(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(instance, FileSelectorActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useAccelerometer = false;
+        config.useCompass = false;
+
+        initialize(app, config);
     }
 }
