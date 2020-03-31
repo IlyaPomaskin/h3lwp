fd965a29c0b32c40806e3382d54c908fd671e93a
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-03-31 11:38:32 +0300

    update deps

--- clj/project.clj
+++ clj/project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [h3m-parser "1.2.3"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [h3m-parser "1.2.4"]]
   :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :global-vars {clojure.core/*warn-on-reflection* true}
