33a7b0540395010ec074414ea5ff87aa632c0d65
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-04-11 17:45:48 +0300

    lower clojure version to 1.8.0

--- clj/project.clj
+++ clj/project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [h3m-parser "1.3.0"]]
-  :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"
+             "-Dclojure.compiler.elide-meta=[:doc :file :line :added]"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :global-vars {clojure.core/*warn-on-reflection* true}
   :aot :all
