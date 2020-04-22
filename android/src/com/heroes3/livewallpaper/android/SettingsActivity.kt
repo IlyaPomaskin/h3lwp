6a95500e54941c67055520f8a02420d33a5a9e7d
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-04-23 00:14:25 +0300

    add settings

---
+++ android/src/com/heroes3/livewallpaper/android/SettingsActivity.kt
@@ -0,0 +1,24 @@
+package com.heroes3.livewallpaper.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import com.heroes3.livewallpaper.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
