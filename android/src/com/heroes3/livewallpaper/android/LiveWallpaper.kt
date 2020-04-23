90566b3fb5a68a76e7bf845c555602ed0d637597
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-04-23 11:04:16 +0300

    add assets parsing button to settings

--- android/src/com/heroes3/livewallpaper/android/LiveWallpaper.kt
+++ android/src/com/heroes3/livewallpaper/android/LiveWallpaper.kt
@@ -4,58 +4,16 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import com.badlogic.gdx.backends.android.AndroidLiveWallpaperService
 
 class LiveWallpaper : AndroidLiveWallpaperService() {
-    /*private var app: LiveWallpaperEngine? = null
-    private val instance = this
-    private val receiver: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val filePath = intent.getStringExtra(INTENT_EXTRA_NAME)
-            println("SELECTED FILE PATH: $filePath")
-            app.selectFile(filePath)
-        }
-    }*/
-
     override fun onCreateEngine(): Engine {
         return AndroidWallpaperEngine()
     }
 
-    /*protected fun checkStoragePermission() {
-        val hasStoragePermission = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-        if (!hasStoragePermission) {
-            app.setStoragePermissionStatus(false)
-        }
-    }*/
-
     override fun onCreateApplication() {
         super.onCreateApplication()
-
-        /*app = LiveWallpaperEngine()
-        app.setFileSelectHandler(Runnable {
-            val intent = Intent(instance, FileSelectorActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
-        })
-        app.setEditPermissionHandler(Runnable {
-            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-            intent.addCategory(Intent.CATEGORY_DEFAULT)
-            intent.data = Uri.parse("package:$packageName")
-            startActivity(intent)
-            checkStoragePermission()
-        })
-        registerReceiver(receiver, IntentFilter(INTENT_NAME))*/
 
         val config = AndroidApplicationConfiguration()
         config.useAccelerometer = false
         config.useCompass = false
         initialize(com.heroes3.livewallpaper.core.Engine(), config)
-    }
-
-    /*override fun onDestroy() {
-        unregisterReceiver(receiver)
-        super.onDestroy()
-    }*/
-
-    companion object {
-        var INTENT_NAME = "selectedFile"
-        var INTENT_EXTRA_NAME = "message"
     }
 }
--- android/src/com/heroes3/livewallpaper/android/SettingsActivity.kt
+++ android/src/com/heroes3/livewallpaper/android/SettingsActivity.kt
@@ -1,7 +1,11 @@
 package com.heroes3.livewallpaper.android
 
+import android.app.Activity
+import android.content.*
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.heroes3.livewallpaper.R
 
@@ -19,6 +23,52 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+
+        override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+            if (preference?.key == "select_file") {
+                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                    .setType("application/octet-stream")
+                    .addCategory(Intent.CATEGORY_OPENABLE)
+                    .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    .setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                    .putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+
+                startActivityForResult(
+                    Intent.createChooser(intent, getString(R.string.assets_select_file_activity_title)),
+                    PICK_FILE_RESULT_CODE
+                )
+
+                return true
+            }
+
+            return super.onPreferenceTreeClick(preference)
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+            super.onActivityResult(requestCode, resultCode, intent)
+
+            if (requestCode == PICK_FILE_RESULT_CODE
+                && resultCode == Activity.RESULT_OK
+                && intent?.data != null) {
+                handleFileSelection(intent.data!!)
+            }
+        }
+
+        private fun handleFileSelection(filePath: Uri) {
+//            val stream = context?.contentResolver?.openInputStream(filePath)
+            println("FILE SELECTED $filePath")
+
+            val parsingProgress = findPreference<Preference>("parsing_progress")
+            if (parsingProgress != null) {
+                parsingProgress.isVisible = true
+                parsingProgress.title = "Parsing..."
+                parsingProgress.summary = "85%..."
+            }
+        }
+
+        companion object {
+            const val PICK_FILE_RESULT_CODE = 1
         }
     }
 }
