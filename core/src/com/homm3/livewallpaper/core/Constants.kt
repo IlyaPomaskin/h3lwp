258aead0fd0c6caf4a8c2aed6c7836b475dcd0b7
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-05-19 14:00:41 +0300

    update instructions

--- core/src/com/homm3/livewallpaper/core/Constants.kt
+++ core/src/com/homm3/livewallpaper/core/Constants.kt
@@ -4,8 +4,9 @@ class Constants {
     companion object {
         const val TILE_SIZE = 32f
         const val FRAME_TIME = 0.18f
-        const val INSTRUCTIONS = "To use this app you must provide files from Heroes 3 SoD or Complete (HD Edition will not work).\n" +
-            "Open the game folder on PC, find folder named  \"Data\", and upload file named \"h3sprite.lod\" to your phone."
+        const val INSTRUCTIONS = "Due to copyright restrictions, we can't provide resources from Heroes 3.\n" +
+            "To use this app you must provide files from your copy of the game. Only Heroes 3 SoD and Complete versions are supported. HD Edition will not work.\n" +
+            "Open the game folder on PC, find a folder named \"Data\", and upload a file named \"h3sprite.lod\" to your phone."
     }
 
     class Preferences {
