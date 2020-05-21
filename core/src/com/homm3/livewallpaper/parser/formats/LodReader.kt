684eb2d91e9f72aa1dcc9bdf3b5fd03171a3bbb3
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-05-21 18:14:08 +0300

    remove unused function

--- core/src/com/homm3/livewallpaper/parser/formats/LodReader.kt
+++ core/src/com/homm3/livewallpaper/parser/formats/LodReader.kt
@@ -58,26 +58,4 @@ internal class LodReader(stream: InputStream) {
             reader.readBytes(file.size).inputStream()
         }
     }
-
-    companion object {
-        fun readFileContent(fileStream: InputStream, file: File): ByteArrayInputStream {
-            val fileContent = ByteArray(file.size)
-            fileStream.reset()
-            fileStream.skip(file.offset.toLong())
-//            fileStream.skip(file.offset - fileStream.channel.position())
-
-            if (file.compressedSize > 0) {
-                val packedData = ByteArray(file.compressedSize)
-                fileStream.read(packedData)
-                val inflater = Inflater()
-                inflater.setInput(packedData)
-                inflater.inflate(fileContent)
-                inflater.end()
-            } else {
-                fileStream.read(fileContent)
-            }
-
-            return fileContent.inputStream()
-        }
-    }
 }
