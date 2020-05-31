c06022af31675a54a859f8644d017a465f34f64a
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2020-05-31 12:20:36 +0300

    remove unused consts

--- core/src/com/homm3/livewallpaper/core/Constants.kt
+++ core/src/com/homm3/livewallpaper/core/Constants.kt
@@ -6,10 +6,6 @@ class Constants {
         const val FRAME_TIME = 0.18f
         const val SCROLL_OFFSET = 1 * TILE_SIZE
         const val VISIBLE_OBJECTS_OFFSET = 6 * TILE_SIZE
-        const val INSTRUCTIONS = "Due to copyright restrictions, we can't provide resources from Heroes 3.\n" +
-            "To use this app you must provide files from your copy of the game. Only Heroes 3 SoD and Complete versions are supported. HD Edition will not work.\n" +
-            "Open the game folder on PC, find a folder named \"Data\", and upload a file named \"h3sprite.lod\" to your phone."
-        const val LOADING_ERROR = "An error happens while loading files.\nTry to clear the cache or reinstall the app.\nIf an error happens again write to me at iph3apps@gmail.com"
     }
 
     class Preferences {
