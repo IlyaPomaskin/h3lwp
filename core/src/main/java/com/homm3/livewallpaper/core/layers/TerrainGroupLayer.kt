d9b452ec9c84acfd28633313aeea7d1e366899d7
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2021-12-05 10:38:02 +0300

    move terrain constants to terrain layer

--- core/src/main/java/com/homm3/livewallpaper/core/layers/TerrainGroupLayer.kt
+++ core/src/main/java/com/homm3/livewallpaper/core/layers/TerrainGroupLayer.kt
@@ -14,7 +14,53 @@ import com.homm3.livewallpaper.core.Constants.Companion.TILE_SIZE
 import com.homm3.livewallpaper.parser.formats.H3m
 import ktx.collections.map
 
-class TerrainGroupLayer(private val assets: Assets, h3m: H3m, isUnderground: Boolean) : MapGroupLayer() {
+class TerrainGroupLayer(private val assets: Assets, h3m: H3m, isUnderground: Boolean) :
+    MapGroupLayer() {
+
+    enum class TerrainDefs(val value: Int) {
+        dirttl(0),
+        sandtl(1),
+        grastl(2),
+        snowtl(3),
+        swmptl(4),
+        rougtl(5),
+        subbtl(6),
+        lavatl(7),
+        watrtl(8),
+        rocktl(9);
+
+        companion object {
+            fun byInt(int: Int): String {
+                return values().find { it.value == int }.toString()
+            }
+        }
+    }
+
+    enum class RiverDefs(val value: Int) {
+        clrrvr(1),
+        icyrvr(2),
+        mudrvr(3),
+        lavrvr(4);
+
+        companion object {
+            fun byInt(int: Int): String {
+                return values().find { it.value == int }.toString()
+            }
+        }
+    }
+
+    enum class RoadDefs(val value: Int) {
+        dirtrd(1),
+        gravrd(2),
+        cobbrd(3);
+
+        companion object {
+            fun byInt(int: Int): String {
+                return values().find { it.value == int }.toString()
+            }
+        }
+    }
+
     enum class TileType {
         TERRAIN,
         RIVER,
@@ -33,21 +79,32 @@ class TerrainGroupLayer(private val assets: Assets, h3m: H3m, isUnderground: Boo
         return when (type) {
             TileType.TERRAIN -> TiledMapTileLayer.Cell().apply {
                 this.tile = createMapTile(
-                    assets.getTerrainFrames(Constants.TerrainDefs.byInt(tile.terrain), tile.terrainImageIndex)
+                    assets.getTerrainFrames(
+                        TerrainDefs.byInt(tile.terrain),
+                        tile.terrainImageIndex
+                    )
                 )
                 this.flipHorizontally = tile.mirrorConfig.get(0)
                 this.flipVertically = tile.mirrorConfig.get(1)
             }
+
             TileType.RIVER -> TiledMapTileLayer.Cell().apply {
                 this.tile = createMapTile(
-                    assets.getTerrainFrames(Constants.RiverDefs.byInt(tile.river), tile.riverImageIndex)
+                    assets.getTerrainFrames(
+                        RiverDefs.byInt(tile.river),
+                        tile.riverImageIndex
+                    )
                 )
                 this.flipHorizontally = tile.mirrorConfig.get(2)
                 this.flipVertically = tile.mirrorConfig.get(3)
             }
+
             TileType.ROAD -> TiledMapTileLayer.Cell().apply {
                 this.tile = createMapTile(
-                    assets.getTerrainFrames(Constants.RoadDefs.byInt(tile.road), tile.roadImageIndex)
+                    assets.getTerrainFrames(
+                        RoadDefs.byInt(tile.road),
+                        tile.roadImageIndex
+                    )
                 )
                 this.flipHorizontally = tile.mirrorConfig.get(4)
                 this.flipVertically = tile.mirrorConfig.get(5)
@@ -67,10 +124,13 @@ class TerrainGroupLayer(private val assets: Assets, h3m: H3m, isUnderground: Boo
             for (y in 0 until mapSize) {
                 val index = tileOffset + mapSize * y + x
                 val tile = h3m.terrainTiles[index]
+
                 terrainLayer.setCell(x, y, createCell(tile, TileType.TERRAIN))
+
                 if (tile.river > 0) {
                     riverLayer.setCell(x, y, createCell(tile, TileType.RIVER))
                 }
+
                 if (tile.road > 0) {
                     roadLayer.setCell(x, y, createCell(tile, TileType.ROAD))
                 }
