7b33e7a35c31460ab57cad3b0a3ab3af9f834a5c
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2022-06-17 14:05:20 +0200

    fix file reading

--- android/src/main/java/com/homm3/livewallpaper/android/data/MapsViewModel.kt
+++ android/src/main/java/com/homm3/livewallpaper/android/data/MapsViewModel.kt
@@ -82,9 +82,6 @@ class MapsViewModel(private val contentResolver: ContentResolver, root: File) : 
 
     fun copyMap(uri: Uri) {
         viewModelScope.launch {
-            _mapReadingErrorState.emit(MapReadingException.CantCopyMap)
-            return@launch
-
             try {
                 val h3mStream = openStream(uri)
                 val h3m = parseMap(h3mStream)
