ae34219d2e23da39c63ab5b0f39f737b8eeab05c
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2022-06-18 23:50:17 +0200

    cleanup

--- android/src/main/java/com/homm3/livewallpaper/android/ui/components/Navigation.kt
+++ android/src/main/java/com/homm3/livewallpaper/android/ui/components/Navigation.kt
@@ -3,11 +3,9 @@ package com.homm3.livewallpaper.android.ui.components
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.homm3.livewallpaper.android.data.MapsViewModel
 import com.homm3.livewallpaper.android.data.ParsingViewModel
 import com.homm3.livewallpaper.android.data.SettingsViewModel
@@ -15,18 +13,13 @@ import com.homm3.livewallpaper.android.data.SettingsViewModel
 object Destinations {
     const val PARSING = "parsing"
     const val SETTINGS = "settings"
-    const val ABOUT = "about"
     const val MAPS = "maps"
-    const val MAP_NAME_KEY = "map_name"
     const val PHONE_LIMITATION = "phone_limitation"
 }
 
 class NavigationActions(navController: NavHostController) {
     val settings: () -> Unit = { navController.navigate(Destinations.SETTINGS) }
-    val about: () -> Unit = { navController.navigate(Destinations.ABOUT) }
     val maps: () -> Unit = { navController.navigate(Destinations.MAPS) }
-    val mapByName: (String) -> Unit =
-        { mapName -> navController.navigate("${Destinations.MAPS}/$mapName") }
     val navigateUp: () -> Unit = { navController.navigateUp() }
     val phoneLimitations: () -> Unit =
         { navController.navigate(Destinations.PHONE_LIMITATION) }
@@ -63,28 +56,8 @@ fun NavigationHost(
             )
         }
 
-        composable(Destinations.ABOUT) {
-            AboutScreen(actions)
-        }
-
         composable(Destinations.MAPS) {
             MapsScreen(viewModel = mapViewModel, actions)
-        }
-
-        composable(
-            "${Destinations.MAPS}/{${Destinations.MAP_NAME_KEY}}",
-            arguments = listOf(
-                navArgument(Destinations.MAP_NAME_KEY) {
-                    type = NavType.StringType
-                }
-            )
-        ) { backStackEntry ->
-            val arguments = requireNotNull(backStackEntry.arguments)
-
-            MapsInfoScreen(
-                mapName = arguments.getString(Destinations.MAP_NAME_KEY, ""),
-                actions
-            )
         }
     }
 }
